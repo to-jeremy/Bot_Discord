c0d4295d27c39ae61aa49662a2e7faa53d9a90d6
TO Jeremy <jeremy.to@etu.sorbonne-universite.fr>
2023-11-27 21:44:23 +0100

    Lister les commandes dans l'odre alphabétique

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ class Commande_Aide(HelpCommand):
         embed = discord.Embed(title="Commandes du Bot", color=0x00ff00)
 
         for cog, commands in mapping.items():
-            command_signatures = [self.get_command_signature(c) for c in commands]
+            command_signatures = [self.get_command_signature(c) for c in sorted(commands, key=lambda x: x.name)]
             if command_signatures:
                 cog_name = getattr(cog, "qualified_name", "Autre")
                 embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
 
         channel = self.get_destination()
         await channel.send(embed=embed)
+
 
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=Commande_Aide())
 
@@ -35,7 +36,8 @@ async def on_ready():
 
 @bot.command(name='afficher_commandes')
 async def afficher_commandes(ctx):
-    command_list = [f'`{command.name}`' for command in bot.commands]
+    # Tri des commandes par nom
+    command_list = [f'`{command.name}`' for command in sorted(bot.commands, key=lambda x: x.name)]
     commands_str = ', '.join(command_list)
     await ctx.send(f'Commandes disponibles : {commands_str}')
 
